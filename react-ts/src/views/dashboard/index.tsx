60619c5dac108ed0e9c034fe6374e1ccca9fa7e2
jingyuan <lujingyuan95@gmail.com>
2022-08-08 17:34:45 +0800

    feat: add fresh button

--- react-ts/src/views/dashboard/index.tsx
+++ react-ts/src/views/dashboard/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { getDashboardInfo } from "@/api/modules/dashboard";
 import "./index.less";
 
-import { Col, Row, Slider } from 'antd';
+import { Col, Row, Button, Divider } from 'antd';
 import React, { useState } from 'react';
 import { Dashboard } from "@/api/interface";
 
@@ -22,8 +22,6 @@ const DashboardInfo: React.FC = () => {
 			const { data } = await getDashboardInfo();
 			console.log("data", data);
 			if (!data) return;
-			colCount = 5
-			console.log("colCount useEffect", colCount);
 			setData(data);
 		} finally {
 			setLoading(false);
@@ -34,11 +32,17 @@ const DashboardInfo: React.FC = () => {
 		getDashboardData();
 	}, []);
 
+	const refreshDashboardList = async () => {
+		await getDashboardData();
+	};
+
 	return (
 		<>
+			<Button className="refresh-button" shape="round" onClick={refreshDashboardList}>
+				Refresh
+			</Button>
+			<Divider />
 			<Row gutter={[gutters, vgutters]}>
-				{/* {cols}
-				{cols} */}
 				<Col key={0} span={24 / colCount}>
 					<div className="card border-secondary">
 						<header className="card-header">
@@ -95,9 +99,9 @@ const DashboardInfo: React.FC = () => {
 							<div className="card-title">Open-source Information <span className="badge badge-pill badge-warning"></span> </div>
 						</header>
 						<div className="card-body">
-							<p>GitHub Address:<a target="_blank" href="https://github.com/jingyuan-git"> System Monitoring Tool</a></p>
-							<p>GitHub Stars: <a href="https://github.com/jingyuan-git/go-gin-api/stargazers"><img alt="GitHub stars" src="https://img.shields.io/github/stars/jingyuan-git/go-gin-api?style=flat-square" /></a></p>
-							<p>GitHub Forks: <a href="https://github.com/jingyuan-git/go-gin-api/network"><img alt="GitHub forks" src="https://img.shields.io/github/forks/jingyuan-git/go-gin-api?style=flat-square" /></a></p>
+							<p>GitHub Address:<a target="_blank" href="https://github.com/jingyuan-git/system-monitoring-tools"> System Monitoring Tool</a></p>
+							<p>GitHub Stars: <a href="https://github.com/jingyuan-git/system-monitoring-tools/stargazers"><img alt="GitHub stars" src="https://img.shields.io/github/stars/jingyuan-git/system-monitoring-tools?style=flat-square" /></a></p>
+							<p>GitHub Forks: <a href="https://github.com/jingyuan-git/system-monitoring-tools/network"><img alt="GitHub forks" src="https://img.shields.io/github/forks/jingyuan-git/system-monitoring-tools?style=flat-square" /></a></p>
 						</div>
 					</div>
 				</Col>
